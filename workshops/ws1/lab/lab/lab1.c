7fb56af1b4e67ff8c0d5440e7e353503c6b7317a
In Tae Chung <itchung@myseneca.ca>
2023-01-20 20:07:27 -0500

    ws1 lab

---
+++ workshops/ws1/lab/lab/lab1.c
@@ -0,0 +1,30 @@
+//
+//  main.c
+//  lab
+//
+//  Created by Intae Chung on 2023-01-20.
+//
+
+#include <stdio.h>
+void title(void);
+void info(void);
+void line(void);
+
+int main(void) {
+    title();
+    line();
+    info();
+    line();
+}
+
+void title(void) {
+    printf("IPC144 NBB\tWorkshop 1\tPart 1\n");
+}
+
+void line(void) {
+    printf("------------------------------------------------------------");
+}
+
+void info(void) {
+    printf("Name:\n\tIn Tae Chung\nEmail:\n\titchung@myseneca.ca\n");
+}
