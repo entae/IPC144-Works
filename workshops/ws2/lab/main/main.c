b84ac219bef614fe63338922007d114cc2d620e7
In Tae Chung <itchung@myseneca.ca>
2023-01-27 17:23:03 -0500

    ws2 lab

--- workshops/ws2/lab/main/main.c
+++ workshops/ws2/lab/main/main.c
@@ -4,14 +4,20 @@
 //
 //  Created by Intae Chung on 2023-01-26.
 //
+#include "stdio.h"
 
 void line(char fill, int length);
-void shoppingStats(void);
+void shoppingStats(int noOfItems);
 
 int main(void) {
-   
-    shoppingStats();
-    line('=',50);
-     
+    int noOfItems;
+    //to track the number of items purchased
+    printf("Money Spending Calculator\n");
+    line('=',25);
+    printf("How many items were bought?\n");
+    printf("> ");
+    scanf("%d", &noOfItems);
+    shoppingStats(noOfItems);
+    
     return 0;
 }
--- workshops/ws2/lab/main/shopping.c
+++ workshops/ws2/lab/main/shopping.c
@@ -8,41 +8,30 @@
 #include "stdio.h"
 void line(char fill, int length);
 
-void title(void) {
-    printf("Money Spending Calculator\n");
-}
-
-void shoppingStats(void) {
-
-    int noOfItems;
-    //the number of items
-    int itm;
-    //to track the number of loops done
+void shoppingStats(int noOfItems) {
+// noOfItem: the number of items purchased
+    int loop;
+    //integer for number of loops
     float price;
     //float value for item prices
-    float total;
+    float sum;
     //float for total price of items
     float avg;
     //float for average price of items
-    
-    printf("Money Spending Calculator\n");
-    line('=',25);
-    printf("How many items were bought?\n");
-    printf("> ");
-    scanf("%d", &noOfItems);
     printf("Please enter the price of the %d items bought...\n",noOfItems);
-    itm = 0;
-    total =0;
-    while (itm != noOfItems) {
-        printf("%d> ", itm+1);
+    loop = 0;
+    sum = 0;
+    while (loop != noOfItems) {
+        printf("%d> ", loop+1);
         scanf("%f", &price);
-        itm++;
-        total += price;
+        loop++;
+        sum += price;
         }
     line('-',50);
-    printf("Total price for all the items purchased: %.2f\n", total);
-    avg = total / noOfItems;
+    printf("Total price for all the items purchased: %.2f\n", sum);
+    avg = sum / noOfItems;
     printf("Average price of the items: %.2f\n",avg);
+    line('=',50);
 }
 
 void line(char fill, int length) {
