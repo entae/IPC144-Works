6d1ec55b927548fe0da8a4cf71881c040e2e0e2d
In Tae Chung <itchung@myseneca.ca>
2023-02-17 21:57:22 -0500

    ws5 lab

--- workshops/ws5/lab/lab/classList.c
+++ workshops/ws5/lab/lab/classList.c
@@ -12,6 +12,8 @@ void printReport(const char subjectCode[], const int studentNumbers[],
                  const int marks[], int noOfStudents) {
     //add four integer variables: a variable for your loop counter, the average mark, lowest mark, and highest mark
     int i, sum = 0, avg, low = 100, high = 0;
+    
+    printf("\n");
     printf("SUBJECT MARKS REPORT!\n"
            "  +---------------+\n");
     printf("  | %-14s|\n", subjectCode);
