4f60aa260df413e90fae407f6139a4830fe9689e
In Tae Chung <itchung@myseneca.ca>
2023-03-24 18:13:35 -0400

    ws6 lab

--- workshops/ws6/ws6/Movie.c
+++ workshops/ws6/ws6/Movie.c
@@ -8,20 +8,20 @@
 #include "Movie.h"
 
 int loadMovie(struct Movie* mptr, FILE* fptr) {
-    return fscanf(fptr, "%60[^\t]\t%d\t%3[^\t]\t%199d\t%60[^\t]\t%f", mptr->title, &mptr->year, mptr->rating, &mptr->minutes, mptr->genre, &mptr->consRating);
+    return (fscanf(fptr, "%60[^\t]\t%d\t%3[^\t]\t%199d\t%60[^\t]\t%f", mptr->title, mptr->year, mptr->rating, mptr->minutes, mptr->genre, mptr->consRating));
 }
 
 void list(const struct Movie* mptr, int row) {
-    printf("|%3d | %20.20s | %4d | %4s | %4d | %25.25s | %4.1f |\n", row, mptr->title, &mptr->year, mptr->rating, &mptr->minutes, mptr->genre, &mptr->consRating);
+    printf("|%3d | %20.20s | %4d | %4s | %4d | %25.25s | %4.1f |\n", row, mptr->title, mptr->year, mptr->rating, mptr->minutes, mptr->genre, mptr->consRating);
 }
 
 void display(const struct Movie* m) {
     printf("Title: %s\n", m->title);
-    printf("Year: %d\n", &m->year);
+    printf("Year: %d\n", m->year);
     printf("Rating: %s\n", m->rating);
-    printf("Duration: %d\n", &m->minutes);
+    printf("Duration: %d\n", m->minutes);
     printf("Genres: %s\n", m->genre);
-    printf("Consumer Rating: %3.lf\n", &m->consRating);
+    printf("Consumer Rating: %3.f\n", m->consRating);
 }
 
 const char* getMovieTitle(const struct Movie* mptr) {
