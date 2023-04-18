b443752114ca5c06721cfb7ec13dd94ecaf8b529
Intae Chung <itchung@myseneca.ca>
2023-04-18 18:58:28 -0400

    MS3

--- Project/MS2/MS2/main.c
+++ Project/MS2/MS2/main.c
@@ -17,20 +17,40 @@
  Classmate Emily Fagin showed me this function as a way of copying one string variable to another without the use of strncpy from the <string.h> library and gave me permission to incorporate it into my code
  */
 /////////////////////////////////////////////////////////////////
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include "PosApp.h"
+#include "PosUI.h"
+void fileDump(const char* filename);
+void ressetData();
 int main() {
-   int i;
-   int foundIndex;
-   loadItems("posdata.csv");
-   for(i = 0; (foundIndex = search()) != -2;i++){
-      if(foundIndex >= 0) {
-         printf("SKU found at index %d\n", foundIndex);
-      }
-      else {
-         printf("SKU not found!\n");
-      }
-   }
-   printf("Search was executed %d times...", i);
+   ressetData();
+   fileDump("posdata.csv");
+   runPos("posdata.csv");
+   fileDump("posdata.csv");
    return 0;
+}
+
+void ressetData() {
+   FILE* f_origin = fopen("posdataOrigin.csv", "r");
+   FILE* f_data = fopen("posdata.csv", "w");
+   char ch;
+   while(fscanf(f_origin, "%c", &ch) == 1) {
+      fprintf(f_data, "%c", ch);
+   }
+   fclose(f_origin);
+   fclose(f_data);
+}
+void fileDump(const char* filename) {
+   FILE* fptr = fopen(filename, "r");
+   if(fptr) {
+      char ch = 0;
+      printf("Contents of file >>%s<<:\n", filename);
+      while(fscanf(fptr, "%c", &ch) == 1) {
+         putchar(ch);
+      }
+      fclose(fptr);
+      printf("End of data in >>%s<<\n", filename);
+   } else {
+      printf("file: >>%s<< not found!\n", filename);
+   }
 }
