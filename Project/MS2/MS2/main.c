938bd760a4499d28ccf7fcd6ec217b6a868bfeba
Intae Chung <itchung@myseneca.ca>
2023-04-18 10:17:23 -0400

    MS31

--- Project/MS2/MS2/main.c
+++ Project/MS2/MS2/main.c
@@ -17,9 +17,30 @@
  Classmate Emily Fagin showed me this function as a way of copying one string variable to another without the use of strncpy from the <string.h> library and gave me permission to incorporate it into my code
  */
 /////////////////////////////////////////////////////////////////
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include "PosUI.h"
+#include "PosApp.h"
+void fileDump(const char* filename);
 int main() {
-   runPos("posdata.csv");
+   fileDump("posdata.csv");
+   loadItems("posdata.csv");
+   saveItems("posdata_out.csv");
+   fileDump("posdata_out.csv");
    return 0;
+}
+
+void fileDump(const char* filename) {
+   FILE* fptr= fopen(filename, "r");
+   if(fptr) {
+      char ch=0;
+      printf("Contents of file >>%s<<:\n",filename);
+      while(fscanf(fptr, "%c", &ch) == 1) {
+         putchar(ch);
+      }
+      fclose(fptr);
+      printf("End of data in >>%s<<\n", filename);
+   }
+   else {
+      printf("file: >>%s<< not found!\n", filename);
+   }
 }
