0bed56022c985aec85cc430b080646f41decca88
Intae Chung <itchung@myseneca.ca>
2023-04-18 17:19:36 -0400

    MS34

--- Project/MS2/MS2/PosApp.c
+++ Project/MS2/MS2/PosApp.c
@@ -128,7 +128,7 @@ int search(void) {
         result = -2;
     }
     for ( i=0; i<noOfItems; i++ ) {
-        if(cpyString(sku, items[i].SKU, MAX_SKU_LEN) == 1) {
+        if(cpyString(sku, items[i].SKU, MAX_SKU_LEN) == i) {
             result = i;
         }
     }
